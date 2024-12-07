36329c285912fea59474168c4a454e5498068be5
Trioproject <longsreynich34@gmail.com>
2024-12-07 15:22:11 +0700

    This is my classwork

---
+++ Navbar/resources/views/About.blade.php
@@ -0,0 +1,11 @@
+
+@extends('layouts.layout')
+@section('content')
+<div class="position-relative">
+  <div class="position-absolute top-60 start-50">
+  <h4>  
+      This is about Page
+  </h4>
+  </div>
+</div>
+@endsection
---
+++ Navbar/resources/views/Home.blade.php
@@ -0,0 +1,12 @@
+
+@extends('layouts.layout')
+
+@section('content')
+<div class="position-relative">
+  <div class="position-absolute top-60 start-50">
+  <h4>  
+      This is Home Page
+  </h4>
+  </div>
+</div>
+@endsection
